b24519fab9251c22d02f610829662d244fae17f7
lance-animoca <lancet@anichess.com>
2023-12-28 12:05:25 +0800

    FIX: update code based on internal audit report

--- contracts/escrow/TokenEscrow.sol
+++ contracts/escrow/TokenEscrow.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {AccessControlStorage} from "@animoca/ethereum-contracts/contracts/access/libraries/AccessControlStorage.sol";
 import {ContractOwnership} from "@animoca/ethereum-contracts/contracts/access/ContractOwnership.sol";
 import {ERC1155TokenReceiver} from "@animoca/ethereum-contracts/contracts/token/ERC1155/ERC1155TokenReceiver.sol";
 import {IERC1155TokenReceiver} from "@animoca/ethereum-contracts/contracts/token/ERC1155/interfaces/IERC1155TokenReceiver.sol";
@@ -10,26 +9,23 @@ import {ForwarderRegistryContext} from "@animoca/ethereum-contracts/contracts/me
 import {ForwarderRegistryContextBase} from "@animoca/ethereum-contracts/contracts/metatx/base/ForwarderRegistryContextBase.sol";
 import {IForwarderRegistry} from "@animoca/ethereum-contracts/contracts/metatx/interfaces/IForwarderRegistry.sol";
 import {Context} from "@openzeppelin/contracts/utils/Context.sol";
-import {ContractOwnershipStorage} from "@animoca/ethereum-contracts/contracts/access/libraries/ContractOwnershipStorage.sol";
-import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import {TokenRecovery} from "@animoca/ethereum-contracts/contracts/security/TokenRecovery.sol";
 
 /// @title TokenEscrow contract
 /// @notice Contract that allows users to escrow tokens for use in the Anichess Game.
-contract TokenEscrow is ForwarderRegistryContext, ContractOwnership, TokenRecovery, ERC1155TokenReceiver {
+contract TokenEscrow is ForwarderRegistryContext, TokenRecovery, ERC1155TokenReceiver {
     /// @notice Emitted when tokens are deposited
-    event DepositToken(address indexed user, uint256 indexed id);
+    event DepositTokens(address indexed user, uint256[] ids);
 
     /// @notice Emitted when tokens are withdrawn
-    event WithdrawToken(address indexed user, uint256 indexed id);
+    event WithdrawTokens(address indexed user, uint256[] ids);
 
     // Custom errors
     error InvalidInventory();
-    error InvalidInputParams();
-    error InsufficientBalance();
-    error InvalidAmount(uint256 tokenId, uint256 amount);
-
-    bytes4 private constant EIP1271_MAGICVALUE = 0x1626ba7e;
+    error UnsupportedMethod();
+    error InsufficientBalance(uint256 tokenId, uint256 balance);
+    error BalanceExceeded(uint256 tokenId, uint256 balance);
+    error InconsistentArrays();
 
     /// @notice An Orb contract reference
     IERC1155 public immutable TOKEN_INVENTORY;
@@ -41,19 +37,24 @@ contract TokenEscrow is ForwarderRegistryContext, ContractOwnership, TokenRecove
     /// @dev Throws if the _inventory address is a zero address.
     /// @dev ContractOwnership is required to initiate TokenRecovery
     /// @param forwarderRegistry The forwarder registry contract information
-    /// @param inventory_ The inventory contract address
-    constructor(
-        IForwarderRegistry forwarderRegistry,
-        address inventory_
-    ) ForwarderRegistryContext(forwarderRegistry) ContractOwnership(_msgSender()) {
-        if (inventory_ == address(0)) {
+    /// @param inventory The inventory contract address
+    constructor(IForwarderRegistry forwarderRegistry, IERC1155 inventory) ForwarderRegistryContext(forwarderRegistry) ContractOwnership(msg.sender) {
+        if (address(inventory) == address(0)) {
             revert InvalidInventory();
         }
-        TOKEN_INVENTORY = IERC1155(inventory_);
+        TOKEN_INVENTORY = inventory;
+    }
+
+    /// @notice Returns the escrowed token balance of a given token held by a given address
+    /// @param account The address of the token holder
+    /// @param id The ID of the token
+    /// @return balance The amount of token escrowed held by the token holder correponding to the token ID
+    function balanceOf(address account, uint256 id) external view returns (uint256) {
+        return escrowedNFTs[account][id];
     }
 
     /// @notice Handles the deposit of tokens.
-    /// @dev Batch transfer token from the operator to this contract
+    /// @dev Batch transfer tokens from the sender to this contract
     /// @param ids An array containing ids of each token being transferred (corresponds to orbTypes)
     /// @param values An array containing amounts of each token being transferred (corresponds to quantities)
     function deposit(uint256[] calldata ids, uint256[] calldata values) external {
@@ -63,55 +64,42 @@ contract TokenEscrow is ForwarderRegistryContext, ContractOwnership, TokenRecove
     /// @notice Handles token withdrawal
     /// @dev Reverts if the array length of ids & values does not match.
     /// @dev Reverts if the sender does not have enough balance.
-    /// @dev Emits a {WithdrawToken} event.
+    /// @dev Updates the escrowedTokens mapping.
+    /// @dev Emits {WithdrawTokens} events.
     /// @dev Transfers the token from this contract to the sender's address
     /// @param ids An array containing ids of each token being transferred (corresponds to orbTypes)
     /// @param values An array containing amounts of each token being transferred (corresponds to quantities)
     function withdraw(uint256[] calldata ids, uint256[] calldata values) external {
         if (ids.length != values.length) {
-            revert InvalidInputParams();
+            revert InconsistentArrays();
         }
-        address operator = _msgSender();
+        address sender = _msgSender();
         for (uint256 i = 0; i < ids.length; i++) {
             uint256 id = ids[i];
             uint256 value = values[i];
-            if (escrowedNFTs[operator][id] < value) {
-                revert InsufficientBalance();
+            uint256 senderBalance = escrowedNFTs[sender][id];
+            if (senderBalance < value) {
+                revert InsufficientBalance(id, senderBalance);
             }
-            escrowedNFTs[operator][id] -= value;
-            emit WithdrawToken(operator, id);
+            escrowedNFTs[sender][id] -= value;
         }
-        TOKEN_INVENTORY.safeBatchTransferFrom(address(this), operator, ids, values, "");
+
+        emit WithdrawTokens(sender, ids);
+
+        TOKEN_INVENTORY.safeBatchTransferFrom(address(this), sender, ids, values, "");
     }
 
     /// @notice Handles the receipt of a single type of token.
-    /// @dev Reverts if the sender is not the inventory.
-    /// @dev Reverts if the value is > 1
-    /// @dev Updates the escrowedNFTs mapping.
-    /// @dev Emits a {DepositToken} event.
-    /// @param from The address which previously owned the token
-    /// @param id The ID of the token being transferred (corresponds to orbType)
-    /// @param value The quantity of the token being transferred
-    /// @return selector The function selector
-    function onERC1155Received(address, address from, uint256 id, uint256 value, bytes calldata) external returns (bytes4) {
-        if (_msgSender() != address(TOKEN_INVENTORY)) {
-            revert InvalidInventory();
-        }
-
-        escrowedNFTs[from][id] += value;
-        if (escrowedNFTs[from][id] > 1) {
-            revert InvalidAmount(id, escrowedNFTs[from][id]);
-        }
-
-        emit DepositToken(from, id);
-        return IERC1155TokenReceiver.onERC1155Received.selector;
+    /// @dev Reverts if this method has been triggered.
+    function onERC1155Received(address, address, uint256, uint256, bytes calldata) external pure returns (bytes4) {
+        revert UnsupportedMethod();
     }
 
     /// @notice Handles the receipt of multiple types of tokens.
     /// @dev Reverts if the sender is not in the inventory.
-    /// @dev Reverts if the updated is > 1
-    /// @dev Updates the escrowedNFTs mapping.
-    /// @dev Emits a {DepositToken} event for each token.
+    /// @dev Updates the escrowedTokens mapping.
+    /// @dev Reverts if the updated token balance is greater than 1
+    /// @dev Emits a {DepositTokens} event.
     /// @param from The address which previously owned the token
     /// @param ids An array containing ids of each token being transferred (corresponds to orbTypes)
     /// @param values An array containing amounts of each token being transferred (corresponds to quantities)
@@ -123,16 +111,18 @@ contract TokenEscrow is ForwarderRegistryContext, ContractOwnership, TokenRecove
         uint256[] calldata values,
         bytes calldata
     ) external returns (bytes4) {
-        if (_msgSender() != address(TOKEN_INVENTORY)) {
+        if (msg.sender != address(TOKEN_INVENTORY)) {
             revert InvalidInventory();
         }
         for (uint256 i = 0; i < ids.length; i++) {
-            escrowedNFTs[from][ids[i]] += values[i];
-            if (escrowedNFTs[from][ids[i]] > 1) {
-                revert InvalidAmount(ids[i], escrowedNFTs[from][ids[i]]);
+            uint256 id = ids[i];
+            escrowedNFTs[from][id] += values[i];
+            uint256 updatedBalance = escrowedNFTs[from][id];
+            if (updatedBalance > 1) {
+                revert BalanceExceeded(id, updatedBalance);
             }
-            emit DepositToken(from, ids[i]);
         }
+        emit DepositTokens(from, ids);
         return IERC1155TokenReceiver.onERC1155BatchReceived.selector;
     }
 
--- contracts/mocks/TokenEscrow/TokenEscrowMock.sol
+++ contracts/mocks/TokenEscrow/TokenEscrowMock.sol
@@ -3,25 +3,23 @@ pragma solidity 0.8.17;
 
 import {TokenEscrow} from "../../escrow/TokenEscrow.sol";
 import {IForwarderRegistry} from "@animoca/ethereum-contracts/contracts/metatx/interfaces/IForwarderRegistry.sol";
-import {ContextMock} from "../utils/ContextMock.sol";
-import {Context} from "@openzeppelin/contracts/utils/Context.sol";
+import {Context} from "@openzeppelin/contracts-4.9.5/utils/Context.sol";
+import {IERC1155} from "@animoca/ethereum-contracts/contracts/token/ERC1155/interfaces/IERC1155.sol";
 
 /// @title ORBNFTMock Contract
 /// @notice Mock contract for testing purposes, extends the ORBNFT contract.
 
-contract TokenEscrowMock is TokenEscrow, ContextMock {
+contract TokenEscrowMock is TokenEscrow {
+    event Data(bytes data);
+
     /// @notice Creates a new escrow contract
     /// @dev Throws if the _inventory address is a zero address.
     /// @dev ContractOwnership is required to initiate TokenRecovery
     /// @param forwarderRegistry The forwarder registry contract information
     /// @param inventory_ The inventory contract address
-    constructor(IForwarderRegistry forwarderRegistry, address inventory_) TokenEscrow(forwarderRegistry, inventory_) {}
+    constructor(IForwarderRegistry forwarderRegistry, IERC1155 inventory_) TokenEscrow(forwarderRegistry, inventory_) {}
 
-    function _msgSender() internal view override(Context, TokenEscrow) returns (address) {
-        return TokenEscrow._msgSender();
-    }
-
-    function _msgData() internal view override(Context, TokenEscrow) returns (bytes calldata) {
-        return TokenEscrow._msgData();
+    function msgData() public {
+        emit Data(_msgData());
     }
 }
