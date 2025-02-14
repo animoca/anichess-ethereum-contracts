2d7a6cb32c51f2e21edb6e3909cf91f8bcc86464
sam-anichess <samyung@anichess.com>
2025-02-14 17:28:39 +0800

    Refactored mock contracts and test cases for reentrancy

--- contracts/mocks/bitmapClaim/BitmapClaimMockReentrancyAttack.sol
+++ contracts/mocks/bitmapClaim/BitmapClaimMockReentrancyAttack.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-import {BitmapClaimMock} from "./BitmapClaimMock.sol";
-import {BitmapClaimMockReentrancyRecipient} from "./BitmapClaimMockReentrancyRecipient.sol";
+import {BitmapClaim} from "../../bitmapClaim/BitmapClaim.sol";
 
-contract BitmapClaimMockReentrancyAttack is BitmapClaimMock {
+contract BitmapClaimMockReentrancyAttack is BitmapClaim {
+    event DeliverCalled(address recipient, uint256 amount);
+
+    bool private enableValidateClaimReentrancy;
+
+    function setEnableValidateClaimReentrancy(bool enabled) external {
+        enableValidateClaimReentrancy = enabled;
+    }
+
     function _validateClaim(address recipient, uint256[] calldata claimBitPositions, bytes calldata validationData) internal override {
-        BitmapClaimMockReentrancyRecipient(recipient).attack(recipient, claimBitPositions, validationData);
+        if (enableValidateClaimReentrancy) {
+            this.claim(recipient, claimBitPositions, validationData);
+        }
+    }
+
+    function _deliver(address recipient, uint256) internal override {
+        uint256[] memory claimBitPositions = new uint256[](1);
+        bytes memory validationData;
+        this.claim(recipient, claimBitPositions, validationData);
     }
 }
