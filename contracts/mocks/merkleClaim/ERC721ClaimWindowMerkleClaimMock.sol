064e2c442a033fdbb117094b834e28dea95addd0
Andy Siu <andys@animocabrands.com>
2025-06-11 16:26:01 +0800

    updated contract to reduce gas usage by adding internal _canClaim function renamed folder pfp to merkleClaim bumped version to 3.2.0

---
+++ contracts/mocks/merkleClaim/ERC721ClaimWindowMerkleClaimMock.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {ERC721ClaimWindowMerkleClaim} from "../../merkleClaim/ERC721ClaimWindowMerkleClaim.sol";
+import {ERC721Full} from "@animoca/ethereum-contracts/contracts/token/ERC721/preset/ERC721Full.sol";
+import {IForwarderRegistry} from "@animoca/ethereum-contracts/contracts/metatx/interfaces/IForwarderRegistry.sol";
+
+contract ERC721ClaimWindowMerkleClaimMock is ERC721ClaimWindowMerkleClaim {
+    constructor(
+        uint256 mintSupply,
+        ERC721Full rewardContract,
+        IForwarderRegistry _forwarderRegistry
+    ) ERC721ClaimWindowMerkleClaim(mintSupply, rewardContract, _forwarderRegistry) {}
+
+    function __msgSender() external view returns (address) {
+        return _msgSender();
+    }
+
+    /// @notice Internal function to access the current msg.data.
+    /// @return The current msg.data value.
+    function __msgData() external view returns (bytes calldata) {
+        return _msgData();
+    }
+}
